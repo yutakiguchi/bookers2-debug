07d500c73c5e788ef546d3c1f5591e63dd428389
EC2 Default User <ec2-user@ip-172-31-41-242.ec2.internal>
2021-05-04 03:34:47 +0000

    [finish4]

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,25 @@ class User < ApplicationRecord
   has_many :books,dependent: :destroy
   has_many :book_coments,dependent: :destroy
   has_many :favorites,dependent: :destroy
+
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction,length: {maximum: 50}
+
+  has_many :reverse_of_relationships,class_name:"Relationship",foreign_key: "followed_id",dependent: :destroy
+  has_many :followers,through: :reverse_of_relationships,source: :follower
+  has_many :relationships,class_name:"Relationship",foreign_key:"follower_id",dependent: :destroy
+  has_many :followings,through: :relationships,source: :followed
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
 end
